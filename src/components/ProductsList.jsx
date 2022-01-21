dfcc2040d49e7f6fbd6e67df67c7e77b597bbd3c
j.kunciene <j.kunciene@gmail.com>
2022-01-21 09:52:59 +0200

    first commit

---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Cart from './Cart';
+import Product from './Product';
+
+const ProductsList = () => {
+
+
+  return (
+
+    <div>
+        <Product/>
+        <Cart/>
+    </div>
+  )
+       
+};
+
+export default ProductsList;
